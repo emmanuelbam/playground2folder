dec644591dc2d542def684160e63bea0fcfaf559
bamidele emmanuel <emmanuelbam9876@gmail.com>
2023-03-20 11:41:43 -0700

    i add javascript

--- playground2.js
+++ playground2.js
@@ -15,22 +15,41 @@ const cvcInp =document.getElementById("cvc-inp")
 
 const btnConfirm =document.getElementById("btn-confirm")
 
-
-
+var regName = /^[a-zA-Z]/
+var regNumber = /^[0-9]/
 
 
 cardholderName.addEventListener("keypress",(e)=>{
-   document.getElementById("card-name-display").textContent  = e.target.value
-    if(checkVals(e)){
+  var user2 = document.getElementById("cardholder-name");
 
-    }
+  var user = document.getElementById("card-name-display").textContent  = e.target.value
+
+   if(regName.test(user)) {
+    user2.style.border = "green solid 5px";
+
+   } else {
+    user2.style.border = "red solid 5px";
+   }
 })
 
 cardNo.addEventListener("keypress",(e)=>{
  
-    document.getElementById("card-no-display").textContent = e.target.value
+    var userno2 = document.getElementById("card-no")
+    
+  var userno =  document.getElementById("card-no-display").textContent = e.target.value
+  
+    if(regNumber.test(userno)) {
+
+       userno2.style.border = "green solid 5px"
+
+    } else {
+        userno2.style.border = "red solid 5px"
+    }
+    
+    
+  
 })
 
-const checkVals = (e) => {
-    return (e.target.value.match(/[A-Za-z]/i  ));
-}
+// const checkVals = (e) => {
+//     return (e.target.value.match(/[A-Za-z]/i  ));
+// }
